d1579198dc6a06a7b34b8e4a4f319d80f31ab5bd
rajatthakur00@gmail.com <rajat@55>
2020-09-20 01:08:36 +0530

    Initial Commit

---
+++ src/main/java/com/example/msscspringconfigserver/msscspringconfigserver/MsscSpringConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.msscspringconfigserver.msscspringconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscSpringConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscSpringConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msscspringconfigserver/msscspringconfigserver/MsscSpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msscspringconfigserver.msscspringconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSpringConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
